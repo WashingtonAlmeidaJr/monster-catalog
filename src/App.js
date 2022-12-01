732435b35b0d4b1077baab44013f28c6d5c87339
WashingtonAlmeidaJr <washingtonjaaj@gmail.com>
2022-11-30 22:38:44 -0300

    Add files via upload
    convertendo o projeto de componets baseados em classes para componetes de função

--- src/App.js
+++ src/App.js
@@ -1,64 +1,47 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { render } from "@testing-library/react";
-import { Component } from "react";
+import React, { useEffect, useState } from "react";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilterdMonsters] = useState(monsters);
 
-    this.state = {
-      monsters: [],
-      searchField: "",
-    };
-  }
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+  //I passed a empty array as dependencie, so anything gonna trigger the function again, it will not fetch again the users, if we didn t use useEffect, the fetch function will enter in a loop and will fetch the data infinite time because the users array outside the browser is always different from the inside users
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
-      response.json().then((users) => {
-        this.setState(
-          () => {
-            return { monsters: users };
-          },
-          () => {}
-        );
-      })
-    );
-  }
-  // this.setState is updating the searchFieldString to be used as a filter in the filteredMonster variable.
-
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
-    this.setState(() => {
-      return { searchField };
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
+    setFilterdMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+  //filter the monster array as the search field value
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
   };
 
-  render() {
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-    // destructuring
+  console.log(filteredMonsters);
 
-    const filteredMonster = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField);
-      // includes is case sensitive, that why we use toLocalLowerCase
-    });
+  return (
+    <div className="App">
+      <h1 className="app-title">Monster Catalog</h1>
 
-    return (
-      <div className="App">
-        <h1 className="app-title">Monster Catalog</h1>
-
-        <SearchBox
-          onChangeHandler={onSearchChange}
-          className="search-box"
-          placeholder="search a monster"
-        />
-        <CardList monsters={filteredMonster} anything={["a", "z"]} />
-      </div>
-    );
-  }
-}
+      <SearchBox
+        className="search-box"
+        placeholder="search a monster"
+        onChangeHandler={onSearchChange}
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
 
 export default App;
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,17 +1,15 @@
 import "./card.styles.css";
 import { Component } from "react";
 
-class Card extends Component {
-    render() {
-    const {id, name, email}=this.props.monster
-return (
+const Card = ({monster}) => {
+  const { id, name, email } = monster;
+  return (
     <div className="card-container" key={id}>
-        <img src={"https://robohash.org/" + id + 100 + "?set=set2"} alt="" />
-        <h1>{name}</h1>
-        <p>{email}</p>
+      <img src={"https://robohash.org/" + id + 100 + "?set=set2"} alt="" />
+      <h1>{name}</h1>
+      <p>{email}</p>
     </div>
-);
-}
-}
+  );
+};
 
 export default Card;
