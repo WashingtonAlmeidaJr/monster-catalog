152735bb720fd756038b1c0f14f27d0a781122d1
Washington <washingtonjaaj@gmail.com>
2022-12-01 08:44:31 -0300

    changing title

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="app-title">Monster Catalog</h1>
+      <h1 className="app-title">Monster Catalog Commit</h1>
 
       <SearchBox
         className="search-box"
